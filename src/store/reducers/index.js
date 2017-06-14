10a8b9454b3efa08e691fcd9bb4fe689e3cead74
dchyslenok <dchyslenok@gmail.com>
2017-06-14 22:33:48 +0300

    seamless-immutable

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,28 +1,29 @@
 import { actionTypes } from '../../actions/types';
+import Immutable from 'seamless-immutable';
 
-const initialState = {
+const initialState = Immutable({
   themeCards: [],
   wordCards: [],
   themeSelectedId: null,
   isLoading: false,
-};
+});
 
-export function root(state = initialState, action) {
-  switch (action.type) {
+export function root(state = initialState, payload) {
+  switch (payload.type) {
     case actionTypes.THEMES_REQUEST_SUCCESS:
-      return { ...state, themeCards: action.themeCards, isLoading: false };
+      return Immutable.merge(state, { themeCards: payload.themeCards, isLoading: false });
 
     case actionTypes.WORDS_REQUEST_SUCCESS:
-      return { ...state, wordCards: action.wordCards, isLoading: false };
+      return Immutable.merge(state, { wordCards: payload.wordCards, isLoading: false });
 
     case actionTypes.THEME_SELECT:
-      return { ...state, themeSelectedId: action.id };
+      return Immutable.set(state, 'themeSelectedId', payload.id);
 
     case actionTypes.THEMES_REQUEST:
-      return { ...state, isLoading: true };
+      return Immutable.set(state, 'isLoading', true);
 
     case actionTypes.WORDS_REQUEST:
-      return { ...state, isLoading: true };
+      return Immutable.set(state, 'isLoading', true);
 
     default:
       return state;
